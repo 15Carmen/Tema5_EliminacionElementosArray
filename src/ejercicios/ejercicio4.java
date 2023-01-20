0422a273a08c482a4c300c0f661eb95291a3d9fa
15Carmen <carmen.martin@iesnervion.es>
2023-01-20 12:15:11 +0100

    (☞ﾟヮﾟ)☞

--- src/ejercicios/ejercicio4.java
+++ src/ejercicios/ejercicio4.java
@@ -1,5 +1,13 @@
 package ejercicios;
 
 public class ejercicio4 {
-    
+    /*
+     * Escribir un programa que genere 20 números enteros aleatorios entre 0 y 10 y los almacene en una tabla. A
+     * continuación, debe solicitar al usuario un número entero. El programa debe eliminar de la tabla todos los
+     * números que sean mayores que un número dado. La tabla no está ordenada ni hay que ordenarla.
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
